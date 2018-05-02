5be95c06ec091cc4e4b93821c9bb5f666f85fe3c
Andreas Ray <andreas.ray@uni-tuebingen.de>
2018-05-02 19:10:31 +0200

    Changes to the HTTP-UI. Better model and display of server status and current configuration.

--- controlpage.js
+++ controlpage.js
@@ -3,22 +3,26 @@
 // ViewModel specification for knockout bindings
 function ControlPageViewModel() {
     var self = this;
-    self.ServerState = ko.observable();
+    self.ServerStatus = ko.observable();
+    self.ServerConfig = ko.observable();
     self.RecordingConfig = ko.observable();
 }
 
-// The actual state of the server
-function ServerState() {
+// The actual status of the server
+function ServerStatus() {
     var self = this;
     self.StateId = ko.observable(-1);
     self.CamList = ko.observableArray();
     self.MicList = ko.observableArray();
     self.Disk = ko.observable(new Disk(0,0));
-    self.SavingLocation = ko.observable(false);
-    self.GStreamer = ko.observable(false);
-    
+    self.SavingLocationOk = ko.observable(false);
+    self.GStreamerOk = ko.observable(false);
+
+    self.SavingLocation = ko.observable("");
+    self.Sid = ko.observable("(unknown subject)");
+
     //self.serverstateBg = ko.computed(function() {
-    //    switch (self.ServerState().StateId()) {
+    //    switch (self.ServerStatus().StateId()) {
     //        case "-1": return '#333'; 
     //        case "0": return '#ccc'; 
     //        case "1": return '#8f9'; 
@@ -29,19 +33,22 @@ function ServerState() {
     self.serverstateBg = ko.computed(function() {
         return '#ccc';
         });
+    
 }
 
-function Camera(cam_id, cam_path, cam_image) {
+function Camera(cam_id, cam_path, cam_image, cfg_record) {
     var self = this;
     self.cam_id = ko.observable(cam_id);
     self.cam_path = ko.observable(cam_path);
     self.cam_image = ko.observable(cam_image);
+    self.cfg_record = ko.observable(cfg_record);
 }
 
-function Microphone(mic_id, mic_path) {
+function Microphone(mic_id, mic_path, cfg_record) {
     var self = this;
     self.mic_id = ko.observable(mic_id);
     self.mic_path = ko.observable(mic_path);
+    self.cfg_record = ko.observable(cfg_record);
 }
 
 function Disk(space_av, space_tot) {
@@ -59,7 +66,7 @@ function RecordingConfig() {
     self.Sid = ko.observable();
 }
 
-// Find a camera id in the ServerState (e.g. for checking if a camera of the configuation is actually available)
+// Find a camera id in the ServerStatus (e.g. for checking if a camera of the configuation is actually available)
 function findCam(serverState,cam_id) {
     retVal = false;
     $.each(serverState().CamList(),function(i,item) {
@@ -70,7 +77,7 @@ function findCam(serverState,cam_id) {
     return retVal;
 }
 
-// Find a microphone id in the ServerState (e.g. for checking if a camera of the configuation is actually available)
+// Find a microphone id in the ServerStatus (e.g. for checking if a camera of the configuation is actually available)
 function findMic(serverState,mic_id) {
     retVal = false;
     $.each(serverState().MicList(),function(i,item) {
@@ -85,7 +92,7 @@ function findMic(serverState,mic_id) {
 // Request and control functions via the http side of the server
 
 // Get state of the server
-function get_state() {
+function get_status() {
 
     var config = {
         url: "http://localhost:8040/request",
@@ -97,18 +104,18 @@ function get_state() {
 
     var done_fct = function(json) {
 
-        CPVM.ServerState(new ServerState());
-        CPVM.ServerState().StateId(0);
+        CPVM.ServerStatus(new ServerStatus());
+        CPVM.ServerStatus().StateId(0);
         $.each(json['Cams'], function() {
-            CPVM.ServerState().CamList.push(new Camera(this.Id,this.Hardware,"image_path"));
+            CPVM.ServerStatus().CamList.push(new Camera(this.Id,this.Hardware,"image_path", false));
         });
         $.each(json['Mics'], function() {
-            CPVM.ServerState().MicList.push(new Microphone(this.Id,this.Hardware));
+            CPVM.ServerStatus().MicList.push(new Microphone(this.Id,this.Hardware,false));
         });
         
-        CPVM.ServerState().Disk(new Disk(json['Disk']['SpaceAvailable'], json['Disk']['SpaceTotal']));
-        CPVM.ServerState().SavingLocation(json['LocationOk']);
-        CPVM.ServerState().GStreamer(json['GStreamerOk']);
+        CPVM.ServerStatus().Disk(new Disk(json['Disk']['SpaceAvailable'], json['Disk']['SpaceTotal']));
+        CPVM.ServerStatus().SavingLocationOk(json['LocationOk']);
+        CPVM.ServerStatus().GStreamerOk(json['GStreamerOk']);
         console.log(json);
     }
 
@@ -128,19 +135,22 @@ function get_config() {
 
     var done_fct = function(json) {
 
-        CPVM.RecordingConfig(new RecordingConfig());
+        CPVM.ServerConfig(new RecordingConfig());
         $.each(json['Cameras'], function(i,item) {
-            camExists = findCam(CPVM.ServerState,item);
-            CPVM.RecordingConfig().RecordCams.push(camExists);
+            camExists = findCam(CPVM.ServerStatus,item);
+            if (camExists) {
+                CPVM.ServerStatus().CamList()[i].cfg_record(true);
+            }
         });
         $.each(json['Microphones'], function(i,item) {
-            micExists = findMic(CPVM.ServerState,item);
-            console.log(micExists)
-            CPVM.RecordingConfig().RecordMics.push(micExists);
+            micExists = findMic(CPVM.ServerStatus,item);
+            if (micExists) {
+                CPVM.ServerStatus().MicList()[i].cfg_record(true);
+            }
         });
         
-        CPVM.RecordingConfig().SavingLocation(json['RecFolder']);
-        CPVM.RecordingConfig().Sid(json['Sid']);
+        CPVM.ServerStatus().SavingLocation(json['RecFolder']);
+        CPVM.ServerStatus().Sid(json['Sid']);
         console.log(json);
     }
 
@@ -162,11 +172,11 @@ function set_config() {
 
         CPVM.RecordingConfig(new RecordingConfig());
         $.each(json['Content']['Cameras'], function(i,item) {
-            camExists = findCam(CPVM.ServerState,item);
+            camExists = findCam(CPVM.ServerStatus,item);
             CPVM.RecordingConfig().RecordCams.push(camExists);
         });
         $.each(json['Content']['Microphones'], function(i,item) {
-            micExists = findMic(CPVM.ServerState,item);
+            micExists = findMic(CPVM.ServerStatus,item);
             console.log(micExists)
             CPVM.RecordingConfig().RecordMics.push(micExists);
         });
@@ -188,7 +198,7 @@ function fail_fct(xhr, status, errorThrown) {
 // Instantiating of the viewmodel and application of the bindings
 
 CPVM = new ControlPageViewModel();
-CPVM.ServerState(new ServerState());
+CPVM.ServerStatus(new ServerStatus());
 CPVM.RecordingConfig(new RecordingConfig());
 
 ko.applyBindings(CPVM);
